c1de34503d73e66f07c09e5bfee5fabafbcb12ce
Zeyu Shao <zeyu.shao@hotmail.com>
2024-11-01 10:31:22 -0400

    Finished lab 7

---
+++ src/app/authors/service/authors.service.ts
@@ -0,0 +1,18 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from "@angular/common/http";
+import {Author} from '../model/author';
+import {Observable} from "rxjs";
+
+const Url = 'http://localhost:8080/books-api/';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthorsService {
+
+  private http: HttpClient = inject(HttpClient);
+
+  public getAuthor(id: string): Observable<Author> {
+    return this.http.get<Author>(Url + 'authors/' + id);
+  }
+}
